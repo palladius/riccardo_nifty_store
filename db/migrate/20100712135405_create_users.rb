5641766b66af89d95164373ba1c12d2ea3590e3e
Riccardo C <rusko@palladius.it>
2010-07-12 14:58:05 +0100

    utenti moridifacti con nome goliardico

--- db/migrate/20100712135405_create_users.rb
+++ db/migrate/20100712135405_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :email
+      t.string :goliardic_name      # proof of concept, utente fatto a manhouse
       t.string :password_hash
       t.string :password_salt
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -9,13 +9,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100712135300) do
+ActiveRecord::Schema.define(:version => 20100712135405) do
 
   create_table "posts", :force => true do |t|
     t.string   "name"
     t.text     "content"
     t.integer  "user_id"
     t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "goliardic_name"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
