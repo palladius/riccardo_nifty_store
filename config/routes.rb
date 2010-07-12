38981519891bd4e6fb079dfb938a3f4d9da1931f
Riccardo C <rusko@palladius.it>
2010-07-12 15:07:24 +0100

    aggiunta rooute di default

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.logout 'logout', :controller => 'sessions', :action => 'destroy'
   map.login 'login', :controller => 'sessions', :action => 'new'
   map.resources :sessions
-
   map.resources :users
-
   map.resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -40,7 +38,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "posts"
 
   # See how all your routes lay out with "rake routes"
 
