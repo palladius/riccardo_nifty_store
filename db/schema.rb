0d144fd9eee910c6ada14d04e51f3f537d7c8937
Riccardo C <rusko@palladius.it>
2010-09-21 16:11:23 +0100

    nuasacciu

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100819182051) do
+ActiveRecord::Schema.define(:version => 20100824154017) do
+
+  create_table "ajax_posts", :force => true do |t|
+    t.string   "subject"
+    t.text     "body"
+    t.datetime "published_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "name"
