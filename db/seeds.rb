37cb1143d022cb37765b4e36d61cf8ef4802c6cd
Riccardo Carlesso <palladiusbonton@gmail.com>
2023-08-07 11:44:30 +0200

    1.1.010-20180207 banalizziamo un po

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,25 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#   
+#
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 
-SEED_VER = '0.9a'
+SEED_VER = '0.10a'
 
-MY_ROLES = %w{ 
+MY_ROLES = %w{
  root
- admin           
- user            
- publisher       
- editor       
-}   
-  
+ admin
+ user
+ publisher
+ editor
+}
+
 # Roles
 MY_ROLES.each{ |role|
   Role.create(:name => role , :notes => "Introduced by rake db:seed (v.#{SEED_VER})", :tags => 'role' )
 }
 
 # USERS
-User.create(:username => "riccardo", :email => "rusko@palladius.it", :goliardic_name => "palladius", :about_you => "io sono Riccardo e se volete sapere di piu cliccate...", :password => 'prova42').save
-User.create(:username => "fabio", :email => "burattino@gmail.com", :goliardic_name => "venerdi", :about_you =>  "Fabiolone e' un programmatore di indubbia fama che ...", :password => 'fabiolone353').save
+User.create(:username => "riccardo", :email => "ruskX@palXXXXX.it", :goliardic_name => "palladius", :about_you => "io sono Riccardo e se volete sapere di piu cliccate...", :password => 'prova123').save
+User.create(:username => "fabio", :email => "buraxxxxo@gmail.com", :goliardic_name => "venerdi", :about_you =>  "Fabiolone e' un programmatore di indubbia fama che ...", :password => 'fabioe123').save
